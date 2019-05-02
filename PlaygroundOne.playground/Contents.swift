7670b77aa167dd053e4da41fee76323168c8413a
Arunabh Das <arunabhdas@gmail.com>
2019-05-02 15:15:13 -0400

    Initial commit

---
+++ PlaygroundOne.playground/Contents.swift
@@ -0,0 +1,19 @@
+import UIKit
+
+var str = "Hello, playground"
+let firstNumber = 4
+let secondNumber = 2
+
+
+let raw = #"You can create "raw"\"plain" strings in Swift 5."#
+let multiline = #"""
+You can create """raw"""\"""plain""" strings
+on multiple lines
+in Swift 5.
+"""#
+print("---------\(multiline)---------")
+
+
+let playerAges = [10, 12, 9, 8, 13]
+
+
